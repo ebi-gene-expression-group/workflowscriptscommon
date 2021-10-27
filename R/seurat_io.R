2152f38ecd23b5b71948d53e4815bda78276966d
Pablo Moreno <pablo.a.moreno@gmail.com>
2021-10-27 21:35:23 +0100

    Formatting and use assay in find variable genes (Seurat 3 write loom)

--- R/seurat_io.R
+++ R/seurat_io.R
@@ -12,13 +12,10 @@
 #'
 #' @export
 load_seurat4_packages_for_format <- function(formats) {
-  if("loom" %in% formats ) {
-    suppressPackageStartupMessages(require(SeuratDisk))
-  }
-  if("singlecellexperiment" %in% formats ) {
+  if ("singlecellexperiment" %in% formats ) {
     suppressPackageStartupMessages(require(scater))
   }
-  if("h5seurat" %in% formats | "anndata" %in% formats) {
+  if ("h5seurat" %in% formats | "anndata" %in% formats | "loom" %in% formats ) {
     suppressPackageStartupMessages(require(SeuratDisk))
   }
 }
@@ -37,18 +34,17 @@ load_seurat4_packages_for_format <- function(formats) {
 #'
 #' @return the fixed seurat object with "NONE" in all columns that had only empty values.
 change_completely_empty_metadata_cols <- function(seurat_object) {
-  so<-seurat_object
-  empty<-c()
+  so <- seurat_object
+  empty <- c()
   for (c in 1:ncol(so@meta.data)) {
-    u<-unique(so@meta.data[ , c])
+    u <- unique(so@meta.data[, c])
     if (length(u) == 1 && (toString(u) == "" || is.na(u))) {
-      #so@meta.data[ , c]<-NULL
-      print(paste0("Column ",colnames(so@meta.data)[c]," is all empty or NA, setting to NONE"))
-      empty<-append(empty, c)
+      print(paste0("Column ", colnames(so@meta.data)[c], " is all empty or NA, setting to NONE"))
+      empty <- append(empty, c)
     }
   }
   if (length(empty) > 0)
-    so@meta.data[ , empty]<-"NONE"
+    so@meta.data[, empty] <- "NONE"
   return(so)
 }
 
@@ -69,21 +65,21 @@ change_completely_empty_metadata_cols <- function(seurat_object) {
 write_seurat4_object <- function(seurat_object, format, output_path, verbose = FALSE, assay="RNA", ...){
   if(format == "loom") {
     as.loom(seurat_object, filename = output_path, verbose = verbose)
-  } else if(format == "seurat" ) {
+  } else if (format == "seurat") {
     saveRDS(seurat_object, file = output_path)
-  } else if(format == "singlecellexperiment") {
+  } else if (format == "singlecellexperiment") {
     saveRDS(as.SingleCellExperiment(seurat_object, assay=assay), file = output_path)
-  } else if(format == "h5seurat") {
+  } else if (format == "h5seurat") {
     SaveH5Seurat(seurat_object, filename = output_path, ...)
-  } else if(format == "anndata") {
+  } else if (format == "anndata") {
     tmpFile <- gsub(pattern = ".h5ad", replacement = ".h5seurat", x = output_path)
     SaveH5Seurat(seurat_object, filename = tmpFile)
     Convert(tmpFile, dest = "h5ad")
-    if(file.exists(tmpFile)) {
+    if (file.exists(tmpFile)) {
       file.remove(tmpFile)
     }
   } else {
-    cat("Format",format,"for output not recognised, failing now.", file = stderr())
+    cat("Format", format, "for output not recognised, failing now.", file = stderr())
     quit(status = 1)
   }
 }
@@ -106,20 +102,21 @@ write_seurat4_object <- function(seurat_object, format, output_path, verbose = F
 #' @examples
 #' write_seurat3_object(tsne_object, format="loom")
 write_seurat3_object <- function(seurat_object, format, output_path, verbose = FALSE, assay="RNA", ...){
-  if(format == "loom") {
-    if (length(seurat_object@assays$RNA@var.features) == 0) {
+  if (format == "loom") {
+    if (length(seurat_object@assays[[assay]]@var.features) == 0) {
       # Find variable features needs to be calculated for a loom export
       # if not already there.
-      seurat_object <- FindVariableFeatures(seurat_object)
+      cat("Writing to Loom requires @assays$",assay,"@var.features to be set... running FindVariableFeatures with defaults.")
+      seurat_object <- FindVariableFeatures(seurat_object, assay=assay)
     }
-    seurat_object<-change_completely_empty_metadata_cols(seurat_object)
+    seurat_object <- change_completely_empty_metadata_cols(seurat_object)
     as.loom(seurat_object, filename = output_path, verbose = verbose, assay = assay, ...)
-  } else if(format == "seurat" ) {
+  } else if (format == "seurat") {
     saveRDS(seurat_object, file = output_path)
-  } else if(format == "singlecellexperiment") {
+  } else if (format == "singlecellexperiment") {
     saveRDS(as.SingleCellExperiment(seurat_object, assay=assay, ...), file = output_path)
   } else {
-    cat("Format",format,"for output not recognised, failing now.", file = stderr())
+    cat("Format", format, "for output not recognised, failing now.", file = stderr())
     quit(status = 1)
   }
 }
@@ -129,7 +126,10 @@ write_seurat3_object <- function(seurat_object, format, output_path, verbose = F
 #' Currently Loom, Seurat (as RDS), Single Cell Experiment, Matrix (as RDS), h5Seurat and AnnData are supported as output formats.
 #' It will fail with an error code 1 if the format is not recognised.
 #'
-#' For using the method, you need to have loaded Seurat 3, scater and loomR
+#' For using the method, you need to have loaded Seurat 3, scater and loomR.
+#'
+#' Currently, due to conflicts in libgfortran version, reading AnnData for
+#' Seurat 4 is not working.
 #'
 #' @param seurat_object The seurat object to be serialised
 #' @param format The file format to write against: "loom", "seurat", "rds_matrix", "anndata" or "singlecellexperiment"
@@ -149,35 +149,38 @@ read_seurat4_object <- function(input_path, format,
   if(format == "loom") {
     loom_object <- Connect(filename = input_path, mode = "r")
     return(as.Seurat(loom_object,
-                     assay=assay, ...))
-  } else if(format == "seurat") {
-    so<-readRDS(file = input_path)
-    if(update_seurat_object) {
-      so<-UpdateSeuratObject(so)
+                     assay = assay, ...))
+  } else if (format == "seurat") {
+    so <- readRDS(file = input_path)
+    if (update_seurat_object) {
+      so <- UpdateSeuratObject(so)
     }
     return(so)
-  } else if(format == "singlecellexperiment") {
-    singlecellexp_object<-readRDS(file = input_path)
+  } else if (format == "singlecellexperiment") {
+    singlecellexp_object <- readRDS(file = input_path)
     return(as.Seurat(singlecellexp_object, assay=assay, ...))
-  } else if(format == "anndata") {
+  } else if (format == "anndata") {
+    # Currently reading AnnData for Seurat 4 is not working due to
+    # conflicts with the libgfortran library between hdf5 lib and R 4.0
+    # But this is the logic that would work.
     # Note that convert relies on the file extension of the given file
-    if(!endsWith(input_path, ".h5ad")) {
+    if (!endsWith(input_path, ".h5ad")) {
       cat("If transforming h5ad AnnData to H5Seurat, file needs to end with .h5ad")
       quit(status = 1)
     }
     # create link with correct extension in working directory
-    Convert(input_path, dest = "h5seurat", overwrite = TRUE)
+    Convert(input_path, dest = "h5seurat", overwrite = TRUE, assay=assay)
     seurat_object <- LoadH5Seurat(sub(".h5ad", "h5seurat", input_path))
-    if(! is.null(ident_for_adata)) {
+    if (!is.null(ident_for_adata)) {
       Idents(seurat_object)<-ident_for_adata
     }
     return(seurat_object)
-  } else if(format == "rds_matrix") {
+  } else if (format == "rds_matrix") {
     return(CreateSeuratObject(readRDS(file = input_path), ...))
-  } else if(format == "h5seurat") {
-    return(LoadH5Seurat(file=input_path, ... ))
+  } else if (format == "h5seurat") {
+    return(LoadH5Seurat(file = input_path, ... ))
   } else {
-    cat("Format",format,"for input not recognised, failing now.", file = stderr())
+    cat("Format", format, "for input not recognised, failing now.", file = stderr())
     quit(status = 1)
   }
 }
@@ -200,13 +203,13 @@ read_seurat4_object <- function(input_path, format,
 read_multiple_seurat4_objects<-function(input_path_list, format = "seurat",
                                         ident_for_adata = "louvain", assay="RNA",
                                         update_seurat_object = FALSE, ...) {
-  if(is.null(input_path_list)) {
+  if (is.null(input_path_list)) {
     # This is a convenience for our automated setup
     warning("Input path list given to read_multiple_seurat4_objects is null, returning a null instead of list.")
     return(NULL)
   }
   # Assumes that all datasets are in the same format, in the future we could check if format is a list or a single value
-  objects_list<-list()
+  objects_list <- list()
   for (input in inputs) {
     seurat_object <- read_seurat4_object(input_path = input, format = format, ...)
     append(objects_list, seurat_object)->objects_list
@@ -245,22 +248,22 @@ read_seurat3_object <- function(input_path, format,
                            mode = "r+",
                            skip.validate = loom_connect_skip_validate)
     return(as.Seurat(loom_object,
-                     assay=assay,
-                     normalized=loom_normalized_path,
-                     scaled=loom_scaled_path, ...))
-  } else if(format == "seurat") {
+                     assay = assay,
+                     normalized = loom_normalized_path,
+                     scaled = loom_scaled_path, ...))
+  } else if (format == "seurat") {
     return(readRDS(file = input_path))
-  } else if(format == "singlecellexperiment") {
-    singlecellexp_object<-readRDS(file = input_path)
+  } else if (format == "singlecellexperiment") {
+    singlecellexp_object <- readRDS(file = input_path)
     return(as.Seurat(singlecellexp_object, assay=assay, ...))
-  } else if(format == "anndata") {
-    seurat_object<-ReadH5AD(file = input_path, ...)
-    if(! is.null(ident_for_adata)) {
-      Idents(seurat_object)<-ident_for_adata
+  } else if (format == "anndata") {
+    seurat_object <- ReadH5AD(file = input_path, ...)
+    if (!is.null(ident_for_adata)) {
+      Idents(seurat_object) <- ident_for_adata
     }
     return(seurat_object)
   } else {
-    cat("Format",format,"for input not recognised, failing now.", file = stderr())
+    cat("Format ", format, " for input not recognised, failing now.", file = stderr())
     quit(status = 1)
   }
 }
